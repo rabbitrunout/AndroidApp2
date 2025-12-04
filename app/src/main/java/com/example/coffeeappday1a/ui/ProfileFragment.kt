2c36b59da9527bff548ffb75d64bd61a3efcca93
Irina Saf <irina.safronova0801@gmail.com>
2025-12-04 01:47:13 -0500

    Added Profile page

--- app/src/main/java/com/example/coffeeappday1a/ui/ProfileFragment.kt
+++ app/src/main/java/com/example/coffeeappday1a/ui/ProfileFragment.kt
@@ -5,20 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.coffeeappday1a.databinding.FragmentSimpleBinding
+import com.example.coffeeappday1a.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
 
-    private var _binding: FragmentSimpleBinding? = null
+    private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        _binding = FragmentSimpleBinding.inflate(inflater, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        binding.textView.text = "Profile (coming soon 👤)"
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.profileName.text = "Irina "
+        binding.profileEmail.text = "user@mail.com"
+        binding.statTotalOrders.text = "Orders: ${CartManager.getTotalQuantity()}"
     }
 
     override fun onDestroyView() {
